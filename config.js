97d0b1403d85b0325276486ed4913dcbac49803c
Nguyenletrungthien <101155715+Nguyenletrungthien@users.noreply.github.com>
2023-04-18 18:43:26 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Chị yêu';
+const giftUrl = 'https://www.instagram.com/49kg1m60/';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng chị yêu';
+const contentCard ='Thay mặt "Chủ tịch nước, Thủ tướng Chính phủ và hàng ngàn tỉ dân", em chúc chị yêu có một ngày sinh nhật thật vui vẻ, hạnh phúc. Chúc chị luôn xinh đẹp, ngày càng thành công hơn trong sự nghiệp. Happy birthday to you!';
+const giftImage = 'nen.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
